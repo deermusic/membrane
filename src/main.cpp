c3cbb7e56a5adf5ed964d046ef7ddb13084adff3
Micha Hanselmann <deermichel@github.com>
2019-08-10 20:11:39 -0700

    update doxygen config

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,9 @@
 // Copyright (c) 2019 Micha Hanselmann
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
